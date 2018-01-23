d45f687ed4d340775a8c8ca7e4422c62fd1cb370
kjunsong <35649658+kjunsong@users.noreply.github.com>
2018-01-23 09:18:24 +0800

    Create 1_2Numpy的创建.py

---
+++ numpy_pandas_matplotlib/1_2Numpy的创建.py
@@ -0,0 +1,22 @@
+"""
+关键字
+array：创建数组
+dtype：指定数据类型
+zeros：创建数据全为0
+ones：创建数据全为1
+empty：创建数据接近0
+arrange：按指定范围创建数据
+linspace：创建线段
+"""
+import numpy as np
+
+#创建数组
+#指定数据dtype
+"""创建特定数据"""
+
+#创建全零数据
+#创建全一数组，同时也能在指定这些特定数据的type
+#创建全空数组，其实每个值都是接近于零的数
+#用arange创建连续数组
+#用reshape改变数据形状
+#用linspace创建线段型数据，同样可以进行reshap
